82a6b72cdadc7a1901edffe2a88043565a7acb71
vinceli1004 <vince.li.ct@gmail.com>
2019-03-29 21:27:56 -0400

    adding base directories and files to project

---
+++ app/src/main/java/com/example/visualvoicemail/views/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.visualvoicemail.views;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+
+}
