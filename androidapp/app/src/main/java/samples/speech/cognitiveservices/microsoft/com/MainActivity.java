7a30eb268023362537dcd85e58d3d5cb2b523fc8
vinceli1004 <vince.li.ct@gmail.com>
2019-03-30 10:56:00 -0400

    get request initialization

--- androidapp/app/src/main/java/samples/speech/cognitiveservices/microsoft/com/MainActivity.java
+++ androidapp/app/src/main/java/samples/speech/cognitiveservices/microsoft/com/MainActivity.java
@@ -13,6 +13,16 @@ import com.microsoft.cognitiveservices.speech.SpeechConfig;
 import com.microsoft.cognitiveservices.speech.SpeechRecognitionResult;
 import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import java.util.concurrent.Future;
 
 import static android.Manifest.permission.*;
